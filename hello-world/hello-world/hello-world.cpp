fda59e4393fe3a7c266991f5cb21da0d0484c3f3
Ali Gholami <aligholami7596@gmail.com>
2018-04-01 23:42:52 +0430

    Important commit

--- hello-world/hello-world/hello-world.cpp
+++ hello-world/hello-world/hello-world.cpp
@@ -7,9 +7,13 @@
 
 int main()
 {
-	omp_set_num_threads(10);
+	// Set the number of threads
+	// Actually it requests this number -> Maybe not available!
+	omp_set_num_threads(10000);
 	#pragma omp parallel
 	{
+		// Get the thread number
+		// 10 Threads will be running the whole block simultaneously
 		int ID = omp_get_thread_num();
 		printf("Hello(%d) ", ID);
 		printf("World(%d) ", ID);
