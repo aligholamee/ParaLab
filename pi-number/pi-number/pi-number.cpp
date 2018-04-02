ca759ecd985bc0c0e4151589d13d68cb6408c0b1
Ali Gholami <aligholami7596@gmail.com>
2018-04-02 14:33:07 +0430

    Fixed pi computations :| Python Ghost was here :|

--- pi-number/pi-number/pi-number.cpp
+++ pi-number/pi-number/pi-number.cpp
@@ -8,13 +8,15 @@ int main()
 	double sum = 0.0;
 	double x = 0, y = 0;
 	int num_steps = 50000;
-	double step_ahead = 1.0 / (double)num_steps;
+	double step_width = 1.0 / (double)num_steps;
 	int i = 0;
 
-	for (i = 0; i < num_steps; i++)
-		x += step_ahead;
+	for (i = 0; i < num_steps; i++) {
+		x += step_width;
 		y = 4.0 / (1.0 + pow(x, 2.0));
-		sum += x * y;
+		sum += step_width * y;
+	}
+
 
 	printf("The computer pi number is: %lf", sum);
 	getchar();
