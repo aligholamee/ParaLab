2931237f0f56da6e5ea0ba9a89fe72d65135d760
Ali Gholami <aligholami7596@gmail.com>
2018-04-03 21:37:25 +0430

    Update parallel algorithm

--- pi-number/pi-number/pi_parallel.c
+++ pi-number/pi-number/pi_parallel.c
@@ -9,24 +9,25 @@
 
 int main() {
 
-	int num_steps = 50000;
+	static long num_steps = 100000;
 	int i = 0;
 
 	// This probably raises the problem of false sharing at the end of the day
 	double sum[NUM_THREADS];
-	double pi = 0.0;
+	double pi;
 	double step_width = 1.0 / (double)num_steps;
-	int ACTUAL_NUM_THREADS = 0;
+	int ACTUAL_NUM_THREADS;
 
-	// Request 5 threads
+	// Request 2 threads
 	omp_set_num_threads(NUM_THREADS);
 
 	#pragma omp parallel
 	{
 		int ID = omp_get_thread_num();
-		int i = 0;
-		double x = 0.0;
-		double y = 0.0;
+		int i;
+		double x = (double)ID;
+		double y;
+		int nthrds;
 
 		ACTUAL_NUM_THREADS = omp_get_num_threads();
 
@@ -38,7 +39,10 @@ int main() {
 
 		// Split the job among the threads
 		for (i = ID, sum[ID] = 0.0; i < num_steps; i += ACTUAL_NUM_THREADS) {
-			x += ACTUAL_NUM_THREADS;
+			// Go to the proper x
+			x = i * step_width;
+
+			// Find the proper y corresponding to that x
 			y = 4.0 / (1.0 + x * x);
 			sum[ID] += step_width * y;
 		}
@@ -48,7 +52,7 @@ int main() {
 	for (i = 0; i < ACTUAL_NUM_THREADS; i++) {
 		pi += sum[i];
 		double temp = sum[i];
-		printf("sum[%d] = %lf\n", i, temp);
+		printf("sum[%d] = %2.4f\n", i, temp);
 	}
 
 	// Display the results
