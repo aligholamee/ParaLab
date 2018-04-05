58abacc366d3d0d9daf2279dfe8b98226ae08611
Ali Gholami <aligholami7596@gmail.com>
2018-04-05 18:17:20 +0430

    Fixed minor bugs

--- pi-number/pi-number/pi_number_reduction.cpp
+++ pi-number/pi-number/pi_number_reduction.cpp
@@ -6,18 +6,17 @@
 int main()
 {
 	double sum = 0.0;
-	double x = 0, y = 0;
+	double x = 0.0, y = 0.0;
 	int num_steps = 50000;
 	double step_width = 1.0 / (double)num_steps;
 	int i = 0;
-
+	
 	#pragma omp parallel for reduction(+:sum)
 	for (i = 0; i < num_steps; i++) {
-		x = (i+0.5) * step_width;
-		y = 4.0 / (1.0 + x*x);
+		x = (i + 0.5) * step_width;
+		y = 4.0 / (1.0 + x * x);
 		sum += step_width * y;
 	}
-
 
 	printf("The computed pi number is: %lf", sum);
   return 0;
