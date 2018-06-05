dbf6cf4840d2ff3232aeb921552aa5399383b8bf
Ali Gholami <aligholami7596@gmail.com>
2018-06-05 23:51:20 +0430

    Update: Final Cuda Kernel Added; Uses Shared Memory

---
+++ CUDA/matrix-multiplication/kernel_update_4.cu
@@ -0,0 +1,459 @@
+// System includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+
+/**
+* Matrix multiplication (CUDA Kernel) on the device: C = A * B
+*/
+#define TILE_WIDTH 16
+
+__global__ void
+matrixMulCUDA(float *C, float *A, float *B, int n)
+{
+	int k;
+
+	// Get the row and the column in which thread resides in a block
+	int row = threadIdx.x;
+	int col = threadIdx.y;	
+	float sum = 0.0f;
+	if (row >= n || col >= n) {
+		return;
+	}
+	for (k = 0; k < n; k++) {
+		sum += A[row * n + k] * B[k * n + col];
+
+	}
+	C[row * n + col] = sum;
+}
+
+__global__ void 
+matrixMulCUDA2(float *C, float *A, float *B, int n)
+{
+	/*
+		Each thread computes more than 1 matrix elements
+	*/
+
+	// Define the starting row and ending row for each thread
+	int startRow = threadIdx.y * TILE_WIDTH;
+	int endRow = startRow + TILE_WIDTH;
+
+	// Define the starting column and ending column for each thread
+	int startCol = threadIdx.x * TILE_WIDTH;
+	int endCol = startCol + TILE_WIDTH;
+
+	// Now we have some block in 2 dimensions
+	for (int row = startRow; row < endRow; row++) {
+		for (int col = startCol; col < endCol; col++) {
+
+			if (row >= n || col >= n) {
+				continue;
+			}
+
+			// Compute the proper sum for each block
+			float sum = 0.0f;	// Defined as a register (Better than directly writing to C)
+			for (int k = 0; k < n; k++) {
+				sum += A[row * n + k] * B[k * n + col];
+			}
+			
+			// Write back sum into C
+			C[row * n + col] = sum;
+		}
+	}
+}
+
+__global__ void
+matrixMulCUDA3(float *C, float  *B, float *A, int n)
+{
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x+ threadIdx.x;
+
+	float sum = 0.0f;
+
+	if (row >= n || col >= n) {
+		return;
+	}
+
+	for (int k = 0; k < n; k++) {
+		sum += A[row * n + k] * B[k * n + col];
+	}
+	C[row * n + col] = sum;
+}
+
+__global__ void
+matrixMulCUDA4(float *C, float *A, float *B, unsigned int n)
+{
+	/*
+	Each block computes a tile
+	*/
+	int tileWidth = 32;
+
+	// Define the starting row and ending row for each thread
+	int startRow = blockIdx.y * blockDim.y + threadIdx.y * tileWidth;
+	int endRow = startRow + tileWidth;
+
+	// Define the starting column and ending column for each thread
+	int startCol = blockIdx.x * blockDim.x + threadIdx.x * tileWidth;
+	int endCol = startCol + tileWidth;
+
+	// Now we have some block in 2 dimensions
+	for (int row = startRow; row < endRow; row++) {
+		for (int col = startCol; col < endCol; col++) {
+
+			if (row >= n || col >= n) {
+				continue;
+			}
+
+			// Compute the proper sum for each block
+			float sum = 0.0f;	// Defined as a register (Better than directly writing to C)
+			for (int k = 0; k < n; k++) {
+				sum += A[row * n + k] * B[k * n + col];
+			}
+
+			// Write back sum into C
+			C[row * n + col] = sum;
+		}
+	}
+}
+
+__global__ void
+matrixMulCUDA5(float *C, float *A, float *B, unsigned int n)
+{
+
+	const int tileWidth = 1;
+
+	// Define the starting row and ending row for each thread block
+	int startRow = blockIdx.y * blockDim.y + threadIdx.y * tileWidth;
+	int endRow = startRow + tileWidth;
+
+	// Define the starting column and ending column for each thread block
+	int startCol = blockIdx.x * blockDim.x + threadIdx.x * tileWidth;
+	int endCol = startCol + tileWidth;
+	 
+	// Each block of threads allocate space on shared memory
+	__shared__ float A_S[32 * 32 * 4];
+	__shared__ float B_S[32 * 32 * 4];
+
+	// Each thread helps copying the proper indexes into the shared memory
+	// Now we have some blocks in 2 dimensions
+	for (int row = startRow; row < endRow; row++) {
+		for (int col = startCol; col < endCol; col++) {
+
+
+
+			// Copy data into shared memory
+			for (int k = 0; k < n; k++) {
+				A_S[k] = A[row * n + k];
+				B_S[k] = B[k * n + col];
+			}
+			
+			// Synchronize all threads to make a tile completely ready to go!
+			__syncthreads();
+
+			// Compute the proper sum for each block
+			float sum = 0.0f;	// Defined as a register (Better than directly writing to C)
+			for (int k = 0; k < n; k++) {
+				sum += A_S[k] * B_S[k];
+			}
+
+			// Write back sum into C
+			C[row * n + col] = sum;
+		}
+	}
+}
+
+
+void sequentialMatMul(float *C, float *B, float *A, unsigned int n)
+{
+	for (int row = 0; row < n; row++) {
+		for (int col = 0; col < n; col++) {
+
+			// Compute the proper sum for each block
+			float sum = 0.0f;
+			for (int k = 0; k < n; k++) {
+				sum += A[row * n + k] * B[k * n + col];
+			}
+
+			// Write back sum into C
+			C[row * n + col] = sum;
+		}
+	}
+}
+
+float computeMeanSquaredError(float *A, float *B, unsigned int n)
+{
+	float sumOfError = 0;
+	for (int i = 0; i < n; i++) {
+		float deviation = A[i] - B[i];
+		deviation *= deviation;
+		sumOfError += deviation;
+	}
+
+	return sumOfError;
+}
+
+void constantInit(float *data, int size, float val)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		data[i] = val;
+	}
+}
+
+void printMatrix(float *c, int size) {
+	printf("----------------------\n");
+	// Print a
+	int i = 0;
+	for (i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			printf("%f ", (c[i*size+j]));
+		}
+		printf("\n");
+	}
+}
+
+/**
+* Run a simple test of matrix multiplication using CUDA
+*/
+int matrixMultiply(int argc, char **argv, int n)
+{
+	// Allocate host memory for matrices A and B
+	unsigned int size_A = n * n;
+	unsigned int mem_size_A = sizeof(float)* size_A;
+	float *h_A = (float *)malloc(mem_size_A);
+	unsigned int size_B = n * n;
+	unsigned int mem_size_B = sizeof(float)* size_B;
+	float *h_B = (float *)malloc(mem_size_B);
+
+	// Initialize host memory
+	const float valB = 1.0f;
+	constantInit(h_A, size_A, 1.0f);
+	constantInit(h_B, size_B, valB);
+
+	// Allocate device memory
+	float *d_A, *d_B, *d_C;
+
+	// Allocate host matrix C
+	unsigned int mem_size_C = n * n * sizeof(float);
+	float *h_C = (float *)malloc(mem_size_C);
+
+	// Allocated for sequential computation
+	float *h_X = new float[mem_size_C];
+
+
+	if (h_C == NULL)
+	{
+		fprintf(stderr, "Failed to allocate host matrix C!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	cudaError_t error;
+
+	error = cudaMalloc((void **)&d_A, mem_size_A);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaMalloc d_A returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	error = cudaMalloc((void **)&d_B, mem_size_B);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaMalloc d_B returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	error = cudaMalloc((void **)&d_C, mem_size_C);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaMalloc d_C returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	// copy host memory to device
+	error = cudaMemcpy(d_A, h_A, mem_size_A, cudaMemcpyHostToDevice);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaMemcpy (d_A,h_A) returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	error = cudaMemcpy(d_B, h_B, mem_size_B, cudaMemcpyHostToDevice);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaMemcpy (d_B,h_B) returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	// Setup execution parameters
+	dim3 gridSize(ceil((float)n/32), ceil((float)n/32), 1);
+	dim3 blockSize(32, 32, 1);
+
+	// Create and start timer
+	printf("Computing result using CUDA Kernel...\n");
+
+	// Allocate CUDA events that we'll use for timing
+	cudaEvent_t start;
+	error = cudaEventCreate(&start);
+
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to create start event (error code %s)!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+	cudaEvent_t stop;
+	error = cudaEventCreate(&stop);
+
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to create stop event (error code %s)!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+	// Record the start event
+	error = cudaEventRecord(start, NULL);
+
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to record start event (error code %s)!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+	// Execute the kernel
+	matrixMulCUDA5 << < gridSize, blockSize >> > (d_C, d_A, d_B, n);
+
+	error = cudaGetLastError(); 
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to launch kernel!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+	// Record the stop event
+	error = cudaEventRecord(stop, NULL);
+
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to record stop event (error code %s)!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+	// Wait for the stop event to complete
+	error = cudaEventSynchronize(stop);
+
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to synchronize on the stop event (error code %s)!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+	float msecTotal = 0.0f;
+	error = cudaEventElapsedTime(&msecTotal, start, stop);
+
+	printf("Elapsed time in msec = %f\n", msecTotal);
+
+	if (error != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to get time elapsed between events (error code %s)!\n", cudaGetErrorString(error));
+		exit(EXIT_FAILURE);
+	}
+
+
+	// Copy result from device to host
+	error = cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaMemcpy (h_C,d_C) returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+		exit(EXIT_FAILURE);
+	}
+
+	// Call serial kernel :D
+	sequentialMatMul(h_X, h_B, h_A, n);
+
+	// Compute the Mean Squared Error
+	float MSE = computeMeanSquaredError(h_X, h_C, n);
+
+	printf("Computed MSE: %lf\n", MSE);
+
+	// printMatrix(h_A, n);
+	// printMatrix(h_B, n);
+	// printMatrix(h_C, n);
+
+	// Clean up memory
+	free(h_A);
+	free(h_B);
+	free(h_C);
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+
+
+	return EXIT_SUCCESS;
+
+}
+
+
+/**
+* Program main
+*/
+int main(int argc, char **argv)
+{
+	printf("[Matrix Multiply Using CUDA] - Starting...\n");
+
+	// By default, we use device 0
+	int devID = 0;
+	cudaSetDevice(devID);
+
+	cudaError_t error;
+	cudaDeviceProp deviceProp;
+	error = cudaGetDevice(&devID);
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaGetDevice returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+	}
+
+	error = cudaGetDeviceProperties(&deviceProp, devID);
+
+	if (deviceProp.computeMode == cudaComputeModeProhibited)
+	{
+		fprintf(stderr, "Error: device is running in <Compute Mode Prohibited>, no threads can use ::cudaSetDevice().\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	if (error != cudaSuccess)
+	{
+		printf("cudaGetDeviceProperties returned error %s (code %d), line(%d)\n", cudaGetErrorString(error), error, __LINE__);
+	}
+	else
+	{
+		printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+	}
+
+	// Size of square matrices
+	size_t n = 0;
+	printf("[-] N = ");
+	scanf("%u", &n);
+
+	printf("MatrixA(%d,%d), MatrixB(%d,%d)\n", n, n, n, n);
+
+
+	int matrix_result = matrixMultiply(argc, argv, n);
+
+	system("pause");
+
+	exit(matrix_result);
+}
